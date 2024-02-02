b5725da714623a5933e16e541e3e2462069c68aa
Mukama <mk.frmh3@gmail.com>
2024-02-02 11:59:38 +0200

    responsiveness & login

---
+++ myBrand2024/js/script.js
@@ -0,0 +1,12 @@
+let topMenu = document.querySelector('.menu-list-top');
+function clickingX_Symbolmenu(){
+    topMenu.style.right="-220px";
+
+}
+
+
+function openMenu(){
+    // remember that we have set the right to be -220px (not showing), then here we will show it.
+    topMenu.style.right="0";
+
+}
